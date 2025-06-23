5ff91db8e3ee96ecfadb3c6a2a7e8908bfbf9518
Carlos Elio <carloseliogonzalezapa1997@gmail.com>
2025-06-22 20:59:26 -0500

    Fail

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
-const port = process.env.PORT || 3000;
+const port=process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist:true
+  }))
+  app.useStaticAssets(join(__dirname,'../public'))
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-  app.useStaticAssets(join(__dirname, '../public'));
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = ['https://deploy-nextjs-pos-six.vercel.app'];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+   app.enableCors({
+    origin: ['https://deploy-nextjs-pos-six.vercel.app'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
-
   await app.listen(port);
 }
 bootstrap();
